40a80b3c2a1b6491ebf34f7f792c32cebf68c376
choitjddn0311 <128563032+choitjddn0311@users.noreply.github.com>
2023-05-16 10:46:33 +0900

    school

---
+++ 230510_IT1-1.py
@@ -0,0 +1,20 @@
+# x = input("X 값을 입력해 주세요. >")
+# y = input("Y 값을 입력해 주세요. >")
+
+# x=int(x)
+# y=int(y)
+
+# for y in range(1,y,1):
+#     print(x, y, x*y)
+
+# for x in range(2,3,1):
+#     for y in range(1,3,1):
+#         print(x, y, x*y)
+
+sc = input("과학 선생님의 성함은?")
+so = input("사회 선생님의 성함은?")
+
+if sc == "송주희" or so == "한봉렬":
+    print("정답입니다!")
+else:
+    print("선생님의 이름을 잘 알자!")
