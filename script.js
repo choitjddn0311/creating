cbfb3f9aa7a62b2907e91387e49afd5ca449edf0
choitjddn0311 <cswcjw311427@naver.com>
2023-06-07 18:11:18 +0900

    feat:html,css update

---
+++ script.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+
+ $("header nav ul.gnb>li").hover(function(){},function()
+{});
+
+});//ready end
+
+
+
+// [웹디자인기능사]01 JUST쇼핑몰 문제풀이 -1   29:44 부터 다시하기 
+// 멈추는 이유:스크립트 공부
